6d5960d7f5bf88f43bf45f7adadecf2ea9ce218b
Andrew Bickerton <andrew.bickerton@gmail.com>
2018-02-03 16:46:50 +0000

    popping the queue next

--- Source/ISource.cs
+++ Source/ISource.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 namespace trial.Source
 {
+    // need to define a Queue that the Source populates/can pop from that can be an interface type
+    // also need to abstract out the 'message/body' that can enqueued/popped off that queue
+     
     public interface ISource
     {
         string Name {get; set;}
